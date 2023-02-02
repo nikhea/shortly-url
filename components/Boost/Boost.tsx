4c194f73e38f11abd15567a0f21b2bb80a3bc17d
nikhea <imonikheaugbodaga@gmail.com>
2023-02-02 03:11:02 +0100

    added boost components

--- components/Boost/Boost.tsx
+++ components/Boost/Boost.tsx
@@ -1,7 +1,23 @@
-import React from "react";
-
+import style from "../../scss/boost.module.scss";
+import Image from "next/image";
+import BoostDesktop from "../../public/images/bg-boost-desktop.svg";
+import BoostMobile from "../../public/images/bg-boost-mobile.svg";
 const Boost = () => {
-  return <div>Boost</div>;
+  console.log(BoostMobile);
+
+  return (
+    <div className={style.bg}>
+      {/* <picture>
+        <source media="(min-width: 768px)" srcSet={BoostDesktop} />
+        <Image src={BoostMobile} alt={BoostMobile.src} />
+      </picture> */}
+
+      <div className="container">
+        <h1>boost your links today</h1>
+        <button className="btn-full">get started</button>
+      </div>
+    </div>
+  );
 };
 
 export default Boost;
---
+++ data/cardsDB.tsx
@@ -0,0 +1,12 @@
+export interface ICardsDB {
+  icon: JSX.Element;
+  title: string;
+  description: string;
+}
+export const CardsDB: ICardsDB[] = [
+  {
+    icon: "",
+    title: "",
+    description: "",
+  },
+];
