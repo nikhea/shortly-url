c57553ab7f4e7364de38874a299784e82b9552f4
nikhea <imonikheaugbodaga@gmail.com>
2023-02-05 21:03:31 +0100

    fix input issuse

--- components/InputForm/input.tsx
+++ components/InputForm/input.tsx
@@ -1,6 +1,5 @@
 import style from "../../scss/InputForm.module.scss";
 import { useState } from "react";
-import { log } from "console";
 
 const input = () => {
   const [urlValue, seturlValue] = useState("");
@@ -8,45 +7,46 @@ const input = () => {
   const [errorMsg, setErrorMsg] = useState("Please add a link");
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    if (urlValue === "" || urlValue === undefined) {
-      setErrors(true);
-      setErrorMsg("Please add a link");
-      return;
-    }
-    const response = await fetch(
-      `https://api.shrtco.de/v2/shorten?url=${urlValue}`
-    );
-    const data = await response.json();
-    if (data.error) {
-      const msg = data.error;
-      setErrors(true);
-      setErrorMsg(msg);
-    }
-    if (data.result) {
-      console.log(data);
-      seturlValue("");
-    }
+    // if (urlValue === "" || urlValue === undefined) {
+    //   setErrors(true);
+    //   setErrorMsg("Please add a link");
+    //   return;
+    // }
+    // const response = await fetch(
+    //   `https://api.shrtco.de/v2/shorten?url=${urlValue}`
+    // );
+    // const data = await response.json();
+    // if (data.error) {
+    //   const msg = data.error;
+    //   setErrors(true);
+    //   setErrorMsg(msg);
+    // }
+    // if (data.result) {
+    //   console.log(data);
+    //   seturlValue("");
+    // }
   };
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
-    setErrors(false);
+    // setErrors(false);
     seturlValue(e.target.value);
   };
 
-  console.log(errorMsg);
+  // console.log(errorMsg);
   return (
     <div className="container relative top-16">
       <div className={style.Formcontainer}>
         <form onSubmit={handleSubmit}>
-          <span className="w-[90%] lg:relative">
+          <span className="w-full lg:w-[90%] lg:relative">
             <input
               onChange={handleChange}
               type="text"
               value={urlValue}
               placeholder="Shorten a link here..."
-              style={{
-                border: errors ? "2px solid red" : null,
-              }}
+              // style={{
+              //   border: errors ? "2px solid red" : "",
+              // }}
             />
+
             <p className="text-red-500 lg:absolute">
               {errors && errorMsg.substring(0, 23)}
             </p>
